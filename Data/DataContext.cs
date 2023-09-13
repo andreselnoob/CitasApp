65166b8a19c37cac8955db99a451afae8a030393
52449 <52449@DESKTOP-G1CLM4D>
2023-09-13 07:58:08 -0600

    Add project files.

---
+++ Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using CitasApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CitasApp.Data;
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+     public DbSet<AppUser> Users { get; set; }  
+}
+
